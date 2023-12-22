d1a46ecfc8418951f89d33e46196e0f93696b46c
Parth <88189173+parth375@users.noreply.github.com>
2023-12-23 00:26:32 +0530

    Add files via upload

---
+++ JavaScript/use_strict.js
@@ -0,0 +1,7 @@
+"use_strict"
+// the purpose of using use strict is not make developer's aware of the error while writing the code and use of correct syntax
+x=10
+// here the variable is not declared but only initialize if you run this it will not show this error in the console of browser
+console.log(x)
+// for this script to run correctly we write  "use_strict"
+var x=10 // correct way
