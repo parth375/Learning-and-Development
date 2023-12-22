4bdf98cdf5dc4f129b33f9314ef726029fddb86c
Parth <88189173+parth375@users.noreply.github.com>
2023-12-23 00:11:17 +0530

    Add files via upload

---
+++ JavaScript/naming_variable.js
@@ -0,0 +1,14 @@
+
+// For naming variable you can only use $ or _ at the start of the variable for example:
+var first_name="Paarth" 
+var $first="Warrior"
+var _second="Soldier"
+
+// Using numbers or any other special character will cause an error for example:
+
+var %third="Happy" //-> this will cause an error
+
+console.log($first)
+console.log(_second)
+console.log(%third)
+
