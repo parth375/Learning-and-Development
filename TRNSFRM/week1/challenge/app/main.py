094d987eeb1435a836f1e47915e710293323319e
parth375 <parthchauhan.375@gmail.com>
2025-12-16 02:51:15 +0530

    global handler added

--- TRNSFRM/week1/challenge/app/main.py
+++ TRNSFRM/week1/challenge/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI,Request
 from routers.routes import router
 from fastapi.responses import JSONResponse
 from core.logging import logger
@@ -9,15 +9,10 @@ app.include_router(router)
 
 logger.info("Server Started...")
 
-
-# @app.exception_handler(Exception)
-# def global_exception_handler(request:Request, exc:Exception)-> JSONResponse:
-#     return JSONResponse(
-#          status_code=503,
-#         content={
-#             "error": "service_unavaiable",
-#             "message": "Something went wrong. Please try again later."
-#         }
-#     )
-
-
+@app.exception_handler(Exception)
+async def global_handler(request:Request,exc:Exception)->JSONResponse:
+    logger.exception(exc)
+    return JSONResponse(
+        status_code=500,
+        content={"Message":"We are facing some issue right now try after sometime"}
+    )
